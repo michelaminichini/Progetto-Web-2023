9ccde759298f16c1ba365353cdc4569077c6f892
vangiu00 <giuliavanni00@gmail.com>
2023-11-14 14:43:06 +0100

    creazione db prova

--- progetto-web/backend/sql/creazione_db_progetto_web.sql
+++ progetto-web/backend/sql/creazione_db_progetto_web.sql
@@ -5,11 +5,11 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema progettocinema
+-- Schema cinema
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema progettocinema
+-- Schema cinema
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `cinema` DEFAULT CHARACTER SET utf8 ;
 USE `cinema` ;
@@ -144,7 +144,7 @@ CREATE TABLE IF NOT EXISTS `cinema`.`biglietti` (
   
  -- Table `cinema`.`tariffe`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cinema`.`biglietti` (
+CREATE TABLE IF NOT EXISTS `cinema`.`tariffe` (
   `idtariffa` INT NOT NULL AUTO_INCREMENT,
   `tipo_tariffa` VARCHAR(100)  NOT NULL,
   `descrizione` VARCHAR (100),
