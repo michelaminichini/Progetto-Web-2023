47ec457aa251495b6e8f48ed54303aa5c2b97bf3
michelaminichini <michelaminichini@gmail.com>
2023-11-15 12:15:21 +0100

    modifiche al backend e a app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express, {Express} from "express"
+import history from "connect-history-api-fallback"
 
 const app: Express = express()
 const port: number = 3000
 
 app.use(history())
 app.use(express.static("public"))
+app.use(express.static("dist-frontend"))
+
 
 
 app.use(function(req, res, next) {
