044a2e3d7a9cecf7d826896d3f632c0def231fee
michelaminichini <michelaminichini@gmail.com>
2023-11-15 11:41:13 +0100

    aggiunto vue-router al frontend e modif. main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
+import { createApp } from "vue"
+import {createRouter, createWebHistory, Router} from "vue-router"
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router:Router = createRouter({
+    history:createWebHistory(),
+    routes:[]
+})
+
+
+createApp(App).use(router).mount('#app')
