67529fcb017c54a902bafdaeb8b0e76143c47fca
vangiu00 <giuliavanni00@gmail.com>
2023-11-16 19:38:54 +0100

    modifica query home page

--- backend/src/controllers/film-controller.ts
+++ backend/src/controllers/film-controller.ts
@@ -3,7 +3,7 @@ import {connection} from "../utils/db"
 
 export async function allFilms(req:Request, res: Response) {
     connection.execute(
-        `SELECT idfilm, titolo, regista, genere, durata, nazione, anno, descrizione, trailer, locandina, lingua, attori, attivo FROM film`,
+        `SELECT idfilm, titolo, regista, genere, durata, nazione, anno, descrizione, trailer, locandina, lingua, attori, attivo FROM film WHERE film.attivo = 1`,
     [],
     function (err, results, fields){
         res.json(results)
