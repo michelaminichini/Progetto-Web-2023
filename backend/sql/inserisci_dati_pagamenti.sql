d3fe9ee313e2cde4c60cfe6539987f2f8ed21559
vangiu00 <giuliavanni00@gmail.com>
2023-11-16 10:42:03 +0100

    riempimento tab big., posti.proiez. e pagam.

--- backend/sql/inserisci_dati_pagamenti.sql
+++ backend/sql/inserisci_dati_pagamenti.sql
@@ -1,7 +1,5 @@
 INSERT INTO `pagamenti` (`idpagamento`, `idtipo_pagamento`, `idbiglietto1`, `importo`) VALUES
 (NULL, 1, 1, 8.50), 
-(NULL, 4, 2, 6.50), 
-(NULL, 6, 3, 8.50), 
-(NULL, 7, 4, 8.50);
+(NULL, 4, 2, 6.50);
 ALTER TABLE `pagamenti`
   MODIFY `idpagamento` int(11) NOT NULL AUTO_INCREMENT;
--- backend/sql/inserisci_dati_postiproiezione.sql
+++ backend/sql/inserisci_dati_postiproiezione.sql
@@ -1,3 +1,7 @@
 INSERT INTO `posti_proiezione` (`idposto`, `idproiezione`, `categoria`, `fila`, `numero`, `occupato`, `speciale`) VALUES
+(NULL, 1, 'NULL', 10, 15, 1, 0),
+(NULL, 1, 'NULL', 15, 20, 1, 0),
+(NULL, 1, 'NULL', 15, 21, 1, 0),
+(NULL, 1, 'NULL', 30, 18, 1, 0);
 ALTER TABLE `posti_proiezione`
   MODIFY `idposto` int(11);
