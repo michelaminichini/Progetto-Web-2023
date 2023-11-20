b67b87fa39f77365bc43662a756e05e47ca02930
vangiu00 <giuliavanni00@gmail.com>
2023-11-21 00:07:21 +0100

    style navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,7 +22,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+          <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
         </li>
         <li class="nav-item">
           <router-link class="nav-link" to="/prossimamente">Prossimamente</router-link>
