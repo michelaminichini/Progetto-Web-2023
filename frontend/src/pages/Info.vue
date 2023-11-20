9560b098b24422e41d892372b9ddb15f8438f949
michelaminichini <michelaminichini@gmail.com>
2023-11-20 23:26:17 +0100

    temporaneo - pagina InfoCinema

--- frontend/src/pages/Info.vue
+++ frontend/src/pages/Info.vue
@@ -1,5 +1,46 @@
 <template>
-    <h1>
-        info
-    </h1>
+    <h1>Benvenuti a GalaxyCineVerse: Dove i Sogni Cinematografici Prendono Vita!</h1>
+
+    <body>
+        <div class="app">
+            <article>
+                <h2>Intraprendi un'odissea cinematografica con GalaxyCineVerse</h2>
+
+                <p>Situtato nel cuore della città, GalaxyCineVerse si erge come un faro dell'intrattenimento, catturando il pubblico con la sua ricca storia e i servizi all'avanguardia. Dall'apertura nel 1985, questo iconico cinema è stato un rifugio per cinefili e pilastro della comunità.</p>
+                <h2>Un Passato Glorioso:</h2>
+                <p>La storia di GalaxyCineVerse inizia quando il visionario regista e imprenditore, Alexander Starlight, decise di portare la magia del cinema in città. Con una passione per la narrazione e un sogno di creare uno spazio dove le persone potessero evadere dalla realtà, ha gettato le basi per quello che sarebbe diventato un rifugio cinematografico.</p>
+                <p>Gli anni iniziali hanno visto GalaxyCineVerse evolversi con il passare del tempo, dall'epoca d'oro della pellicola alla rivoluzione digitale. Ha superato alti e bassi del settore, adattandosi sempre alle nuove tecnologie e alle tendenze emergenti. Oggi, rappresenta una testimonianza della duratura potenza del cinema in un mondo in continua evoluzione.</p>
+
+                <h2>Strutture All'Avanguardia:</h2>
+                <p>GalaxyCineVerse vanta strutture all'avanguardia progettate per elevare l'esperienza di visione. Con sistemi audio all'ultimo grido, immagini cristalline e comodi posti a sedere, ogni visita è un viaggio nel cuore dell'eccellenza cinematografica. Il cinema ospita diverse sale, ognuna dedicata a mostrare un'ampia scelta di film.</p>
+                <p>Per coloro che cercano un tocco di lusso, GalaxyCineVerse offre sale VIP con servizi premium, garantendo un'esperienza veramente coinvolgente e indulgente. Il cinema è anche attrezzato con le ultime tecnologie in 3D e IMAX, permettendo agli spettatori di immergersi in mondi al di là dell'immaginazione.</p>
+
+                <h2>L'esperienza GalaxyCineVerse</h2>
+                <p>Al cuore di GalaxyCineVerse c'è l'impegno a offrire più di semplici film; si tratta di creare ricordi.</p>
+                <p>GalaxyCineVerse comprende anche l'importanza dell'accessibilità, offrendo servizi di prenotazione online. Con un'applicazione web, gli spettatori possono pianificare senza sforzo le loro avventure cinematografiche, assicurandosi i posti migliori con pochi tocchi.</p>
+                <p>In conclusione, GalaxyCineVerse fu creato come un posto dove la magia della narrazione incontra la tecnologia all'avanguardia. Con la sua ricca storia, strutture di prima classe e impegno a offrire un'esperienza impareggiabile, GalaxyCineVerse è più di un semplice cinema; è una destinazione per coloro che cercano di essere trasportati in mondi sia familiari che fantastici.</p>
+                <p>Fai parte anche tu dell'esperienza GalaxyCineVerse! Dove ogni fotogramma racconta una storia e ogni visita è un'avventura.</p>
+            </article>
+        </div>
+    </body>
+
+
 </template>
+
+<style scoped>
+h1{
+    color:aliceblue;
+    margin-left: 10%;
+}
+
+article {
+    color:aliceblue;
+    font-size: medium;
+}
+
+#app{
+    
+    margin-left: 10%;
+}
+
+</style>
