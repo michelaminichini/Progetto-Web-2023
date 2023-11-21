f2dda9f6477a65eaae2fd48a09845694f8e762e3
vangiu00 <giuliavanni00@gmail.com>
2023-11-21 13:12:21 +0100

    riempimento provvisorio pag login

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,18 +1,10 @@
 <template>
     <h2>Login</h2>
-    <form action="#" method="POST">
-      <ul>
-        <li>
-          <label for="username">Username:</label>
-          <input type="text" id="username" name="username" />
-        </li>
-        <li>
-          <label for="password">Password:</label>
-          <input type="password" id="password" name="password" />
-        </li>
-        <li>
-          <input type="submit" name="submit" value="Invia" />
-        </li>
-      </ul>
-    </form>
+    
   </template>
+
+<style>
+h2{
+  color: aliceblue;
+}
+</style>
