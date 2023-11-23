7fe5c33521946a115334fd9dd293aca8bb46c1d6
Michela <146772281+michelaminichini@users.noreply.github.com>
2023-11-23 15:36:07 +0100

    rimozione sql file unici pt2

--- backend/sql/inserisci_dati_biglietti.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `biglietti` (`idbiglietto`, `tipo_pagamento`, `idproiezione1`, `idutente`, `idposto1`, `rating`) VALUES
-(NULL, 1, 1, 1, 1, 5),
-(NULL, 4, 1, 2, 2, 3);
-ALTER TABLE `biglietti`
-  MODIFY `idbiglietto` int(11) NOT NULL AUTO_INCREMENT;
