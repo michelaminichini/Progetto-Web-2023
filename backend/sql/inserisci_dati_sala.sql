105601cbde834de39ec7e60f3b27346834a3baf4
Michela <146772281+michelaminichini@users.noreply.github.com>
2023-11-23 15:38:08 +0100

    rimozione file sql singoli pt7

--- backend/sql/inserisci_dati_sala.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `sale` (`idsala`, `descrizione`, `posti`, `posti_speciali`,`fila`, `posti_fila`, `tecnologia`) VALUES
-(NULL,'Sala digitale', 260, 4, 13, 20, 'Dolby Digital 3D, SDDS, DTS'),
-(NULL, 'Sala digitale', 140, 4, 7, 20, 'Dolby Digital EX, SDDS, DTS'),
-(NULL, 'Sala digitale', 225, 4, 15,15, 'Dolby Digital EX, SDDS, DTS');
-ALTER TABLE `sale`
-  MODIFY `idsala` int(11) NOT NULL AUTO_INCREMENT;
