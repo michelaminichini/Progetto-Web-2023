5a5e9a1a3094daac56ae6ff0149585030aa66231
Michela <146772281+michelaminichini@users.noreply.github.com>
2023-11-23 15:37:00 +0100

    rimozione file sql singoli pt4

--- backend/sql/inserisci_dati_pagamenti.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `pagamenti` (`idpagamento`, `idtipo_pagamento1`, `idbiglietto1`, `importo`) VALUES
-(NULL, 1, 1, 8.50), 
-(NULL, 4, 2, 6.50);
-ALTER TABLE `pagamenti`
-  MODIFY `idpagamento` int(11) NOT NULL AUTO_INCREMENT;
