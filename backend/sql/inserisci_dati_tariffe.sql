250cc6fa29d1195ae9ee004b562445ab69e95bc7
Michela <146772281+michelaminichini@users.noreply.github.com>
2023-11-23 15:38:32 +0100

    rimozione file sql singoli pt9

--- backend/sql/inserisci_dati_tariffe.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `tariffe` (`idtariffa`, `tipo_tariffa`, `descrizione`, `validità`, `prezzo`) VALUES
-(NULL, 'Biglietto Intero', 'NULL', 'Tutto il giorno', 8.50),
-(NULL, 'Biglietto Ridotto', 'Bambini fino ai 12 anni, Over 60, Militari, Invalidi, Giornalisti', 'Tutti i giorni', 6.50),
-(NULL, 'Intero 3D', 'Proiezioni 3D', 'Tutto il giorno', 10.50),
-(NULL, 'Ridotto 3D', 'Proiezioni 3D', 'Tutto il giorno', 6.50),
-(NULL, 'Universitari', 'NULL', 'Ogni lunedì, esclusi festivi e prefestivi', 5.50),
-(NULL, 'Ridotto Mercoledì', 'NULL', 'Esclusi festivi e prefestivi', 6.50);
-ALTER TABLE `tariffe`
-  MODIFY `idtariffa` int(11) NOT NULL AUTO_INCREMENT;
