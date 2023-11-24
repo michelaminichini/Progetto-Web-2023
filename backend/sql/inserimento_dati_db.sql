d7d80196a206457e17bd40fb4c27935559648bc3
vangiu00 <giuliavanni00@gmail.com>
2023-11-24 14:35:48 +0100

    add proiezione film id4

--- backend/sql/inserimento_dati_db.sql
+++ backend/sql/inserimento_dati_db.sql
@@ -67,7 +67,7 @@ INSERT INTO `proiezioni` (`idproiezione`, `idfilm`, `idsala`, `datap`, `orario` 
 (NULL, 3, 3, '2023-11-18', '21:00'),
 (NULL, 1, 3, '2023-11-13', '21:00'),
 (NULL, 5, 2, '2023-09-15', '17:00'),
-(NULL, 6, 3, '2023-08-23', '21:00'),
+(NULL, 6, 4, '2023-08-23', '21:00'),
 (NULL, 7, 1, '2023-11-13', '21:00');
 ALTER TABLE `proiezioni`
   MODIFY `idproiezione` int(15) NOT NULL AUTO_INCREMENT;
