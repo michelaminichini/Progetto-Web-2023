502a9fecd02e430ff6e2795cfc05c7cfb3f5a2ab
michelaminichini <michelaminichini@gmail.com>
2023-12-03 15:14:45 +0100

    aggiornato navbar App.vue - responsive

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <header>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
     <h1>GalaxyCineVerse</h1>
   </header>
-  <body>
-            
+  
+  <body> 
     <nav class="navbar navbar-expand-lg bg-transparent">
       <div id="nav" class="container-fluid">
         <a title="GalaxyCineverse" class="navbar-brand" href="#">
           <img src="/img/ImmLogo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
         </a>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
+          <i class="fas fa-bars mobile-nav-icon" @click="toggleMobileNav" style="font-size:36px; color:white"></i>
+          <ul class="navbar-nav" :class=" {'show-mobile-nav': isMobileNavOpen} ">
             <li class="nav-item">
               <router-link class="nav-link" aria-current="page" to="/">Programmazione</router-link>
             </li>
@@ -20,15 +28,11 @@
             <li class="nav-item">
               <router-link class="nav-link" to="/info">Info</router-link>
             </li>
-            <!--li class="nav-item">
-              <router-link class="nav-link" to="/admin">Admin</router-link>
-            </li>-->
+            
             <!-- <li class="nav-item">
               <router-link class="nav-link" to="/seatbooking">SeatBooking</router-link>
             </li> -->
-            <!--li class="nav-item">
-              <router-link class="nav-link" to="/pagamento">Pagamento</router-link>
-            </li>-->
+            
             <li class="nav-item">
               <router-link class="nav-link" to="/paginaAdmin">Pagina Admin</router-link>
             </li>
@@ -37,10 +41,11 @@
             <form class="d-flex">
                 <router-link to="/login"><button class="btn btn-lg btn-primary">LOGIN</button></router-link>
             </form>
-        </div>
+      </div>
+      
+      
     </nav> 
 
-            
     <main>
       <RouterView />
     </main>
@@ -53,7 +58,69 @@
 
 </template>
 
+<script lang="ts">
+
+export default {
+  data() {
+    return {
+      isMobileNavOpen: false,
+    };
+  },
+  mounted() {
+    // Set isMobileNavOpen to false initially
+    this.isMobileNavOpen = false;
+    
+    // Check the screen width and update isMobileNavOpen
+    this.updateMobileNavState();
+    
+    // Listen for window resize events to update isMobileNavOpen
+    window.addEventListener('resize', this.updateMobileNavState);
+  },
+  beforeDestroy() {
+    // Remove the resize event listener to avoid memory leaks
+    window.removeEventListener('resize', this.updateMobileNavState);
+  },
+  methods: {
+    updateMobileNavState() {
+      // Check the screen width and update isMobileNavOpen
+      this.isMobileNavOpen = window.innerWidth <= 600;
+    },
+    toggleMobileNav() {
+      this.isMobileNavOpen = !this.isMobileNavOpen;
+    },
+  },
+};
+
+</script>
 
 <style>
 @import "./styles/style.scss";
+
+
+@media only screen and (max-width: 600px) {
+  nav{
+    display: block;
+  }
+
+  ul {
+     
+    flex-direction: column;
+    position: absolute;
+    top: 60px; /* Adjust as needed */
+    left: 0;
+    background-color: #333;
+    width: 100%;
+  }
+
+  ul.show-mobile-nav {
+    display: flex;
+  }
+
+  li {
+    margin: 10px 0;
+  }
+}
+
+
+
 </style>
