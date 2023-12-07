fa0459bf9dce005a3313d3006fb2464d63b57034
michelaminichini <michelaminichini@gmail.com>
2023-12-07 11:35:00 +0100

    sistemato nav nella App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,63 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      isMobileNavOpen: true,
+    }
+  },
+  mounted() {
+    // Set isMobileNavOpen to false initially
+    //this.isMobileNavOpen = false;
+    
+    // Check the screen width and update isMobileNavOpen
+    this.updateMobileNavState();
+    
+    // Listen for window resize events to update isMobileNavOpen
+    window.addEventListener('resize', this.updateMobileNavState);
+
+    //this.isMobileNavOpen = window.innerWidth <= 600;
+    // Check if the media query is active initially
+    this.logMediaQueryStatus();
+
+    // Listen for window resize events to log changes in media query status
+    window.addEventListener('resize', this.logMediaQueryStatus);
+  },
+  beforeDestroy() {
+    // Remove the resize event listener to avoid memory leaks
+    window.removeEventListener('resize', this.updateMobileNavState);
+  },
+  methods: {
+    updateMobileNavState() {
+      // Check the screen width and update isMobileNavOpen
+     
+      if (window.innerWidth <= 600){
+        this.isMobileNavOpen = false
+      } else if(window.innerWidth > 600){
+        this.isMobileNavOpen = true
+      }
+    },
+    toggleMobileNav() {
+      //console.log('Clicked');
+      this.isMobileNavOpen = !this.isMobileNavOpen;
+      console.log('isMobileNavOpen:', this.isMobileNavOpen);
+    },
+    // Checks for errors in the console log
+    logMediaQueryStatus() {
+    const isMediaQueryActive = window.matchMedia('(max-width: 600px)').matches;
+
+    if (isMediaQueryActive) {
+      console.log('Media query is active');
+    } else {
+      console.log('Media query is not active');// if it is more than 600
+    }
+  },
+  },
+
+})
+</script>
+
 <template>
   <header>
     <link
@@ -18,7 +78,7 @@
         </a>
         <div id="navbarNav" class="d-flex" style="margin-right: 50%;">
           <i class="fas fa-bars mobile-nav-icon" @click="toggleMobileNav" style="font-size:36px; color:white"></i>
-          <ul :class=" {'show-mobile-nav': isMobileNavOpen} ">
+          <ul :style="{ display: isMobileNavOpen ? 'flex' : 'none' }">
             <li class="nav-item" style="margin-right: 5%;">
               <router-link class="nav-link" aria-current="page" to="/">Programmazione</router-link>
             </li>
@@ -60,67 +120,23 @@
 
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  data() {
-    return {
-      isMobileNavOpen: false,
-    }
-  },
-  mounted() {
-    // Set isMobileNavOpen to false initially
-    this.isMobileNavOpen = false;
-    
-    // Check the screen width and update isMobileNavOpen
-    this.updateMobileNavState();
-    
-    // Listen for window resize events to update isMobileNavOpen
-    window.addEventListener('resize', this.updateMobileNavState);
-  },
-  beforeDestroy() {
-    // Remove the resize event listener to avoid memory leaks
-    window.removeEventListener('resize', this.updateMobileNavState);
-  },
-  methods: {
-    updateMobileNavState() {
-      // Check the screen width and update isMobileNavOpen
-      this.isMobileNavOpen = window.innerWidth <= 600;
-    },
-    toggleMobileNav() {
-      this.isMobileNavOpen = !this.isMobileNavOpen;
-    },
-  },
-
-})
-  
-
-
-</script>
-
 
 <style>
 @import "./styles/style.scss";
 
-
 @media only screen and (max-width: 600px) {
   nav{
     display: block;
+    background-color: #333;
   }
 
   ul {
-     
     flex-direction: column;
     position: absolute;
     top: 60px; /* Adjust as needed */
     left: 0;
     background-color: #333;
     width: 100%;
-  }
-
-  .show-mobile-nav ul {
-    display: flex;
   }
 
   li {
