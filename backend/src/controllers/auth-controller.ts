f686e5d365228db144b3dddab4b1092006f880b4
michelaminichini <michelaminichini@gmail.com>
2023-12-10 20:00:07 +0100

    sistemato pulsante registrati

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -5,11 +5,12 @@ import { decodeAccessToken, deleteAccessToken, setAccessToken } from "../utils/a
 //import { Connection } from "mysql2/promise";
 
 
-
+// Procedura di registrazione di un nuovo account 
 export const register = async (req: Request, res: Response) => {
-  //estrarre username (email) e password dal body
+    //estrarre username (email) e password dal body
     const { email, password } = req.body
-  //verificare che la mail sia disponibile
+
+    //verificare che la mail sia disponibile
     const connection = await getConnection()
     const [users] = await connection.execute(`SELECT email FROM utente WHERE email=?`, [email])
     if (Array.isArray(users) && users.length > 0){
@@ -19,17 +20,13 @@ export const register = async (req: Request, res: Response) => {
 
     const passwordHash = await bcrypt.hash(password, 10)
 
+    // Inserire nuova riga nel database contenente email e password dell'utente (password cryptata). In questo modo i dati vengono salvati e l'utente ha creato così il proprio account
     await connection.execute(`INSERT INTO utente (email, password) VALUES (?, ?)`, [email, passwordHash])
-    const [result] = await connection.execute(`SELECT idutente, email, ruolo FROM utente WHERE email=?`, [email])
-
-    const newUser = (result as any)[0]
-
-    setAccessToken(req,res,newUser)
 
     res.json({message: "Registrazione effettuata"})
-    
 }
 
+// Procedura di login al proprio account
 export const login = async (req: Request, res: Response) => {
     // Blocca la richiesta se l'utente ha già effettuato il login
     const user = decodeAccessToken(req, res)
@@ -72,11 +69,11 @@ export const login = async (req: Request, res: Response) => {
     // Crea un JWT contenente i dati dell'utente e lo imposta come cookie
     setAccessToken(req, res, userData)
     console.log("User:", userData)
-    res.json(userData)
+    res.json(userData) // restituisce in console i dati dell'utente che ha eseguito il login
     
 }
 
-
+// Procedura di logout dal proprio account
 export const logout = async (req: Request, res: Response) => {
   // Blocca la richiesta se l'utente non ha effettuato il login
   const user = decodeAccessToken(req, res)
@@ -95,45 +92,3 @@ export const getProfile = async (req: Request, res: Response) => {
   const user = decodeAccessToken(req, res)
   res.json(user)
 }
-
-/*
-
-export const userRole = async (req: Request, res: Response) => {
-  try {
-    // Assuming you have a middleware or authentication logic to get the current user
-    // For simplicity, this example assumes a user object is attached to the request
-    //const currentUser: any = req['user']; // Assuming the user is attached to the request
-
-    //if (!currentUser) {
-      //res.status(401).json({ error: 'User not authenticated' });
-      //return;
-    //}
-    const { email, password, ruolo } = req.body
-    const connection = await getConnection();
-    
-    // Assuming your User entity has a "role" field
-    const [user] = await connection.execute("SELECT ruolo FROM utente WHERE email=?", [email]);
-
-    //if (user) {
-      
-
-      // Check the user role and redirect accordingly
-      if (ruolo === 'amministratore') {
-        res.json({ ruolo });
-        // Redirect admin to the admin page
-        res.redirect('/provaGiulia');
-      } else if (ruolo === 'acquirente') {
-        res.json({ ruolo });
-        // Redirect user to the user page
-        res.redirect("/PostiSala/:idproiezione");
-      } else {
-        // Handle other roles or invalid roles
-        res.status(403).json({ error: 'Invalid user role' });
-      }
-      
-  } catch (error) {
-    console.error('Error fetching user role:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-}
-*/
