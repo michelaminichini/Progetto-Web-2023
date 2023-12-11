9e0637861772d9b7e2a38d2f137991fec305e51b
michelaminichini <michelaminichini@gmail.com>
2023-12-11 15:16:17 +0100

    modificata registrazione utente, resa completa

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -7,8 +7,8 @@ import { decodeAccessToken, deleteAccessToken, setAccessToken } from "../utils/a
 
 // Procedura di registrazione di un nuovo account 
 export const register = async (req: Request, res: Response) => {
-    //estrarre username (email) e password dal body
-    const { email, password } = req.body
+    //estrarre data dal body
+    const { email, password, nome, cognome, telefono, data_nascita } = req.body
 
     //verificare che la mail sia disponibile
     const connection = await getConnection()
@@ -21,7 +21,7 @@ export const register = async (req: Request, res: Response) => {
     const passwordHash = await bcrypt.hash(password, 10)
 
     // Inserire nuova riga nel database contenente email e password dell'utente (password cryptata). In questo modo i dati vengono salvati e l'utente ha creato così il proprio account
-    await connection.execute(`INSERT INTO utente (email, password) VALUES (?, ?)`, [email, passwordHash])
+    await connection.execute(`INSERT INTO utente (email, password, nome, cognome, telefono, data_nascita) VALUES (?, ?, ?, ?, ?, ?)`, [email, nome, cognome, telefono, data_nascita, passwordHash])
 
     res.json({message: "Registrazione effettuata"})
 }
