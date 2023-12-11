3891b35f17201f244b32d50a73fc99be7966515d
vangiu00 <giuliavanni00@gmail.com>
2023-12-11 16:10:11 +0100

    query per tab cronologia (pag utente)

--- backend/sql/query.sql
+++ backend/sql/query.sql
@@ -1,26 +1,11 @@
--- --------------------------------------------------------------------
--- Query 1: quando si apre il sito mostra i film in programmazione (attivo = 1) 
--- --------------------------------------------------------------------
-SELECT titolo, regista, genere, durata, attori, locandina,
-FROM film
-WHERE film.attivo = 1
-
--- Query mostra data e orari di programmazione per il film con id=1
-SELECT datap, orario
-FROM proiezioni JOIN film ON (proiezioni.idfilm = film.idfilm)
-WHERE film.attivo = 1 AND film.idfilm = 1
-
 
 -- --------------------------------------------------------------------
--- Query 2: quando si clicca su scheda film mostra la scheda del film selezionato con i dettagli 
+-- Query per riempire la tabella cronologia nella pag utente
 -- --------------------------------------------------------------------
-SELECT titolo, titolo originale, regista, cast, durata, genere, nazione, anno, trama, trailer, datap, orario
-FROM film JOIN proiezioni ON (film.idfilm = proiezioni.idfilm)
-
--- --------------------------------------------------------------------
--- Query 3: quando si clicca su scegli posto 
--- --------------------------------------------------------------------
-SELECT idposto, occupato, speciale
-FROM posti_proiezione
- 
+SELECT idutente, film.idfilm, titolo, idbiglietto, idtipo_pagamento1, importo, idproiezione, idsala, datap, orario 
+FROM biglietti 
+JOIN pagamenti ON (biglietti.idbiglietto = pagamenti.idbiglietto1) 
+JOIN proiezioni ON (biglietti.idproiezione1 = proiezioni.idproiezione)
+JOIN film ON (proiezioni.idfilm = film.idfilm) 
+WHERE idutente = 1
 
