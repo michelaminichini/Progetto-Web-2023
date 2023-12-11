23cbe6777dda9fa448984ca6c126c2c2dfd12237
vangiu00 <giuliavanni00@gmail.com>
2023-12-11 17:18:59 +0100

    changes in db  x query cronologia utente

--- backend/sql/inserimento_dati_db.sql
+++ backend/sql/inserimento_dati_db.sql
@@ -55,9 +55,9 @@ ALTER TABLE `tariffe`
   MODIFY `idtariffa` int(11) NOT NULL AUTO_INCREMENT;
   
 INSERT INTO `sale` (`idsala`, `descrizione`, `posti`, `posti_speciali`, `tecnologia`) VALUES
-(NULL,'Sala digitale', 260, 4, 'Dolby Digital 3D, SDDS, DTS'),
-(NULL, 'Sala digitale', 140, 4, 'Dolby Digital EX, SDDS, DTS'),
-(NULL, 'Sala digitale', 225, 4, 'Dolby Digital EX, SDDS, DTS');
+(NULL,'Sala digitale A', 260, 4, 'Dolby Digital 3D, SDDS, DTS'),
+(NULL, 'Sala digitale B', 140, 4, 'Dolby Digital EX, SDDS, DTS'),
+(NULL, 'Sala digitale C', 225, 4, 'Dolby Digital EX, SDDS, DTS');
 ALTER TABLE `sale`
   MODIFY `idsala` int(11) NOT NULL AUTO_INCREMENT;
   
