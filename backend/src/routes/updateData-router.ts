09c12440f2bc1d26775f14641e69bbbdad38cba9
vangiu00 <giuliavanni00@gmail.com>
2023-12-17 19:55:03 +0100

    change input metodi di pagamento (non funziona)

--- backend/src/routes/updateData-router.ts
+++ backend/src/routes/updateData-router.ts
@@ -6,5 +6,6 @@ const router = Router()
 router.post("/api/aggiornapagamento", updateDataController.aggiornaDatiPagamentoUtente)
 router.get("/api/leggiutente/:id", updateDataController.leggiDatiProfiloPersonale)
 router.put("/api/aggiornautente", updateDataController.aggiornaDatiProfilo)
+router.get("/api/tipopag", updateDataController.leggiTipoPag)
 
 export default router
