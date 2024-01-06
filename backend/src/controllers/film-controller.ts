0539bde8f732e078e75713c1bd40344fea0338d6
vangiu00 <giuliavanni00@gmail.com>
2024-01-06 18:00:33 +0100

    elenco proiezioni in ordine dallla data più recente

--- backend/src/controllers/film-controller.ts
+++ backend/src/controllers/film-controller.ts
@@ -32,7 +32,7 @@ export async function prossimiFilm(req:Request, res: Response) {
 export async function scheda(req:Request, res:Response) {
     const connection = await getConnection()
     const [results] = await connection.execute(
-        `SELECT film.idfilm, film.titolo, film.locandina, film.regista, film.genere, film.anno, film.descrizione, p.idproiezione, p.datap, p.orario FROM film JOIN proiezioni p ON film.idfilm = p.idfilm WHERE film.idfilm=?`, 
+        `SELECT film.idfilm, film.titolo, film.locandina, film.regista, film.genere, film.anno, film.descrizione, p.idproiezione, p.datap, p.orario FROM film JOIN proiezioni p ON film.idfilm = p.idfilm  WHERE film.idfilm=? ORDER BY datap,orario`, 
         [req.params.id],
     )
     res.json(results)
